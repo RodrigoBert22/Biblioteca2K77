87e2dc256978ae8d7537b44419ebbaed83d457ec
RodrigoBert22 <rodrigobertolini12@hotmail.com>
2018-11-06 21:38:29 -0200

    Index - Login
    Página de Login c/ CSS [v0.0.1]

---
+++ index.php
@@ -0,0 +1,44 @@
+<html>
+  <head>
+
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<link rel="stylesheet" href="css/custom.css">
+
+  </head>
+<body id="LoginForm">
+<div class="container">
+<h1 class="form-heading">Biblioteca</h1>
+<div class="login-form">
+<div class="main-div">
+    <div class="panel">
+   <h2>Acesse o Sistema</h2>
+   <p>Insira o Email e a senha</p>
+   </div>
+
+    <form id="Login">
+
+        <div class="form-group">
+           <input type="email" class="form-control" id="inputEmail" placeholder="Email">
+        </div>
+
+        <div class="form-group">
+
+            <input type="password" class="form-control" id="inputPassword" placeholder="Senha">
+
+        </div>
+        <div class="forgot">
+        <a href="esqueceu.html">Esqueceu a senha?</a>
+</div>
+        <button type="submit" class="btn btn-primary">Login</button>
+
+    </form>
+    </div>
+</div>
+</div>
+</div>
+
+
+</body>
+</html>
