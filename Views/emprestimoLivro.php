7c3ac39dad281881ea519627fecbd09d5aa97e4f
RodrigoBert22 <rodrigobertolini12@hotmail.com>
2018-11-21 23:48:24 -0200

    Emprestimo - Hotfix
    Fix - Disponibilidade Livro

--- Views/emprestimoLivro.php
+++ Views/emprestimoLivro.php
@@ -1,19 +1,55 @@
-<link rel="stylesheet" type="text/css" href="../Content/css/custom.css">
- <div class="principal-emprestimo">
-		 <div class="container-emprestimo">
-			 <div class="after-container-emprestimo">
-			 	 <h1 class="titulo-emprestimo">Emprestimo de Livros</h1>
+<link rel="stylesheet" type="text/css" href="Content/css/custom.css">
+ <div class="principal-emprestimo" style="justify-content: center;">
+     <div class="container-emprestimo">
+       <div class="after-container-emprestimo">
+         <h1 class="titulo-emprestimo">Emprestimo de Livros</h1>
           <form>
             <div class="form-group">
               <label for="email">Nome Do Livro</label>
-              <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Ex: Harry Porra e a Piroca Filosofal">
-              <label for="exampleInputPassword1">Disponibilidade</label>
-              <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Ex: Indisponivel, Disponivel - 10">
-            </div>
-            <div class="btn-emprestimo-checar">
-               <button type="submit" class="btn btn-primary">Checar</button>
-            </div>
+              <select style="width: 500px" class="form-control" name="emprestimoCombo" id="emprestimoCombo">
+   <?php  
+
+       $conn = new mysqli("localhost", "root", "", "biblioteca");
+       
+      // $consulta = mysqli_query($conn, "SELECT idLivro, nomeLivro, COUNT(nomeLivro) FROM livro GROUP BY nomeLivro");
+         // echo "<option id='opt' value='0'> Selecione um Livro </option>";
+       echo "<option id='opt' value= '0'> Selecione o Livro </option>";
+       foreach ( $conn->query('SELECT idLivro, nomeLivro, COUNT(nomeLivro) AS COUNT FROM livro WHERE disponibilidade = 1 GROUP BY nomeLivro') as $row ) {
+          echo "<option id='opt' value= ".$row['nomeLivro']."> ".$row['nomeLivro']." </option>";
+      } 
+
+   ?> 
+</select>  
+
+<div id="show" class="show">
+</div>
+
+
+<script type="text/javascript">
+  var conceptName = $('#emprestimoCombo').val();
+  console.log(conceptName);
+
+  $(document).ready(function(){ 
+      $("#emprestimoCombo").change(function(){ 
+        var conceptName = $(this).val();
+        var allbooks = $(this).val(); 
+        var dataString = "allbooks="+allbooks+"&nomeLivro="+conceptName; 
+
+        $.ajax({ 
+         type: "POST", 
+          url: "Views/get-data.php", 
+          data: dataString, 
+          success: function(result){ 
+            $("#show").html(result);
+          }
+      });
+
+    });
+  }); 
+
+</script>
+
           </form>
-			 </div>
- 		 </div>
+       </div>
+     </div>
  </div>
---
+++ Views/get-data.php
@@ -0,0 +1,15 @@
+<?php
+if(!empty($_POST["allbooks"])){
+
+	$conn = new mysqli("localhost", "root", "", "biblioteca");
+
+  $consulta123 = mysqli_query($conn, "SELECT idLivro, COUNT(nomeLivro) as COUNT FROM livro WHERE nomeLivro LIKE '%".$_POST["nomeLivro"]."%'");
+  echo "<label for=disp>Disponibilidade</label>";
+       while($livrose = mysqli_fetch_object($consulta123)):
+            // echo "<option value='$livrose->COUNT'>".$livrose->COUNT."</option>";
+             echo "<input type='text' class='form-control' id='dist' name='dist' value='".$livrose->COUNT."'>";
+
+       endwhile;
+}
+
+?>
