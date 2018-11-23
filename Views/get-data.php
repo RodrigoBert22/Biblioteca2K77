e844bdfe3cbc9072133c9dda366585cc0316a06f
RodrigoBert22 <rodrigobertolini12@hotmail.com>
2018-11-23 14:34:51 -0200

    Fix SELECT Empréstimo
    Correção na Busca de Livros na Tela de Empréstimo

--- Views/get-data.php
+++ Views/get-data.php
@@ -3,10 +3,9 @@ if(!empty($_POST["allbooks"])){
 
 	$conn = new mysqli("localhost", "root", "", "biblioteca");
 
-  $consulta123 = mysqli_query($conn, "SELECT idLivro, COUNT(nomeLivro) as COUNT FROM livro WHERE nomeLivro LIKE '%".$_POST["nomeLivro"]."%'");
+  $consulta123 = mysqli_query($conn, "SELECT idLivro, COUNT(nomeLivro) as COUNT FROM livro WHERE nomeLivro LIKE '".$_POST["nomeLivro"]."%'");
   echo "<label for=disp>Disponibilidade</label>";
        while($livrose = mysqli_fetch_object($consulta123)):
-            // echo "<option value='$livrose->COUNT'>".$livrose->COUNT."</option>";
              echo "<input style='width: 500px' disabled type='text' class='form-control' id='dist' name='dist' value='".$livrose->COUNT."'>";
 
        endwhile;
